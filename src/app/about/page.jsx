3a95dfea14810fd0b94cd8c6b56a0c095097048a
Brand Gains <brandgains@gmail.com>
2023-11-09 12:31:14 +0530

    Some temporary barriers removed

--- src/app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-import React, {useEffect, useState} from "react";
-import { useRouter } from "next/navigation";
-
-export default function About() {
-return(
-    <></>
-)
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import Image from 'next/image'
+import React from 'react';
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/Hero'
 import AboutBrand from '../components/AboutBrand'
