2288177557cc6895b653159b4e592d30403a9776
Brand Gains <brandgains@gmail.com>
2023-11-13 22:13:50 +0530

    img changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
