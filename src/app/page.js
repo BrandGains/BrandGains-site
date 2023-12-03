c1cf344afc22d1b27b75799d91d166dde12c089d
Brand Gains <brandgains@gmail.com>
2023-12-03 10:22:32 +0530

    Goggle Analytics Setup Done

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Wizards from "@/components/Wizards";
 import Call from "@/components/Call";
+import Script from "next/script";
 
 export const metadata = {
   title: "Brand-Gains | E-commerce Service Agency",
-  description: "We assist e-commerce companies with managing their social media accounts so they can concentrate on expanding their enterprises.",
-}
+  description:
+    "We assist e-commerce companies with managing their social media accounts so they can concentrate on expanding their enterprises.",
+};
 
 export default function Home() {
   return (
@@ -18,6 +20,18 @@ export default function Home() {
       <Services />
       {/* <Wizards /> */}
       <Call />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+    `}
+      </Script>
     </div>
   );
 }
