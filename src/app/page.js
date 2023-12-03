eda6adae306b8b589cb2af1b91dd333b5eda7a19
Brand Gains <brandgains@gmail.com>
2023-12-03 11:27:08 +0530

    Microsoft Clarity Analytics Setup Done

--- src/app/page.js
+++ src/app/page.js
@@ -15,11 +15,27 @@ export const metadata = {
 export default function Home() {
   return (
     <div>
+      {/* Microsoft Clarity */}
+      <Script
+        id="clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_KEY}");
+          `,
+        }}
+      />
       <Hero />
       <Feature />
       <Services />
       {/* <Wizards /> */}
       <Call />
+
+      {/* Google Analytics */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
