26ebc146cb069e471dc0439d949aa0781b36030e
Shahnawaz Ali <shahnawaz132007@gmail.com>
2024-03-02 19:42:02 +0530

    Added Yt iframe in service section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -75,63 +75,27 @@ const Services = () => {
   return (
     <>
       {/* SERVICES SECTION */}
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-            <h1 className="text-7xl font-semibold text-gray-900">
-              Our Services
-            </h1>
-            <p className="lg:w-1/2 text-2xl font-medium w-full leading-relaxed text-gray-500">
-              Explore the range of services we offer to elevate your business.
+      <section className="w-full flex justify-center">
+        <div className="sm:w-4/5 w-11/12 flex justify-evenly gap-20 flex-col sm:flex-row">
+          <div className="video sm:w-fit w-fit h-fit  rounded-lg ">
+            <iframe
+              width="326"
+              height="580"
+              className="rounded-xl"
+              src="https://www.youtube.com/embed/JjzEGstr-Kg?autoplay=1"
+              title="Did You Caught All These Details in The Spider-Man into the Spider-Verse?..."
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="content flex flex-col sm:w-3/5 w-full justify-center gap-10 p-8 rounded-lg shadow-2xl shadow-blue-700 bg-gradient-to-r from-blue-800 to-indigo-900 ">
+            <h2 className="sm:text-7xl text-4xl font-extrabold text-slate-100 tracking-wide w-full">
+              Editing?
+            </h2>
+            <p className="text-xl font-medium text-slate-200">
+              We are <span className="text-2xl">Here!!</span>
             </p>
-          </div>
-          <div className="flex flex-wrap -m-4">
-            {servicesData.map((service, index) => (
-              <div key={index} className="xl:w-1/3 md:w-1/2 p-4">
-                <div className="border border-gray-200 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
-                  <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                    <Image
-                      src={service.icon}
-                      alt={service.title}
-                      width={100}
-                      height={100}
-                      className="w-full h-full object-cover rounded-full"
-                    />
-                  </div>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                    {service.title}
-                  </h2>
-                  <p className="leading-relaxed text-base">
-                    {service.description}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-          <Link href="/Contact">
-            <button className="flex mx-auto mt-16 bg-blue-400 border border-black hover:shadow-md hover:shadow-zinc-400 text-zinc-800 py-2 px-4 focus:outline-none rounded-xl text-md">
-              Contact us
-            </button>
-          </Link>
-        </div>
-        <div className="flex justify-center items-center">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            {socialMediaIcons.map((icon, index) => (
-              <Link href={icon.page} key={index} target="_blank">
-                <div
-                  key={index}
-                  className="bg-white rounded-md p-2 shadow-md flex items-center justify-center mb-4 mx-auto"
-                >
-                  <Image
-                    src={icon.icon}
-                    alt={icon.name}
-                    width={10}
-                    height={10}
-                    className="w-10 h-10 m-2"
-                  />
-                </div>
-              </Link>
-            ))}
           </div>
         </div>
       </section>
